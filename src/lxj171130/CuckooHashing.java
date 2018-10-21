15a20b926da40f6fc62a57b5500475ddb6ca9a18
Leejia James <leejiaj@gmail.com>
2018-10-21 15:47:39 -0500

    rehash changes, added readme

--- src/lxj171130/CuckooHashing.java
+++ src/lxj171130/CuckooHashing.java
@@ -1,8 +1,12 @@
+/**
+ * @author Leejia James
+ * @author Nirbhay Sibal
+ *
+ * Comparison of hashing implementations
+ *
+ * Ver 1.0: 2018/10/20
+ */
 package lxj171130;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class CuckooHashing<T> {
 	
@@ -18,7 +22,7 @@ public class CuckooHashing<T> {
 		tableNum = 1;
 		maxLocations = 4194304; //power of 2, 2^22
 		numHashFunctions = 2;
-		threshold = maxLocations/2;
+		threshold = maxLocations;
 		table = new Object[tableNum][maxLocations];
 		for(int i=0; i<tableNum; i++) {
 			for(int j=0; j<maxLocations; j++) {
@@ -27,6 +31,13 @@ public class CuckooHashing<T> {
 		}
 	}
 	
+	/**
+	 * Calculates and returns hash code
+	 * 
+	 * @param i
+	 * @param x
+	 * @return hashcode
+	 */
 	private int hashCodeCuckoo(int i, T x) {
 		switch(i) {
 			case 1:
@@ -36,10 +47,21 @@ public class CuckooHashing<T> {
 		}
 	}
 	
+	/**
+	 * Code extracted from Java’s HashMap
+	 * @param h
+	 * @param length
+	 * @return index
+	 */
 	private int indexFor(int h, int length) { // length = table.length is a power of 2
 		return h & (length-1);
 	}
 	
+	/**
+	 * Code extracted from Java’s HashMap
+	 * @param h
+	 * @return hashcode
+	 */
 	private int hash(int h) {
 		// This function ensures that hashCodes that differ only by
 		// constant multiples at each bit position have a bounded
@@ -48,6 +70,12 @@ public class CuckooHashing<T> {
 		return h ^ (h >>> 7) ^ (h >>> 4);
 	}
 	
+	/**
+	 * Add element x to the hashtable
+	 * 
+	 * @param x
+	 * @return true if element is added, false if element is not added
+	 */
 	public boolean add(T x) {
 		for(int i=1; i<=numHashFunctions; i++ ) {
 			if(table[0][hashCodeCuckoo(i,x)].equals(x)) {
@@ -74,20 +102,45 @@ public class CuckooHashing<T> {
 				table[0][location] = temp;
 			}
 			i = i == numHashFunctions ? 1 : (i+1);
-			count++;
 		}
+		
 		//Rebuild hash table with new hash functions;
-		maxLocations = 2 * maxLocations;
-		Object[][] table1 = new Object[tableNum][maxLocations];
-		for(int i1=0; i1<tableNum; i1++) {
-			for(int j=0; j<maxLocations/2; j++) {
-				table1[i1][j] = table[i1][j];
-			}
-		}
-		table = table1;
-		return false;
+		rehashTable();
+		add(x);
+		return true;
 	}
 	
+	/**
+	 * Rehashing table when hashing could not find location in table 
+	 * even after reaching threshold
+	 */
+	private void rehashTable() {
+		Object[][] table1 = new Object[tableNum][maxLocations];
+		for(int i1=0; i1<tableNum; i1++) {
+			for(int j=0; j<maxLocations; j++) {
+				table1[i1][j] = table[i1][j];
+				table[i1][j] = minIntValue;
+			}
+		}
+		maxLocations = 2 * maxLocations;
+		table = new Object[tableNum][maxLocations];
+		for(int i1=0; i1<tableNum; i1++) {
+			for(int j=0; j<maxLocations; j++) {
+				table[i1][j] = minIntValue;
+			}
+		}
+		for(int i1=0; i1<tableNum; i1++) {
+			for(int j=0; j<maxLocations/2; j++) {
+				add((T) table1[i1][j]);
+			}
+		}
+	}
+
+	/**
+	 * Checks if table contains the element x
+	 * @param x
+	 * @return true if element is present in hashtable, false otherwise
+	 */
 	public boolean contains(T x) {
 		for(int i=1; i<=numHashFunctions; i++ ) {
 			if(table[0][hashCodeCuckoo(i,x)].equals(x)) {
@@ -97,6 +150,11 @@ public class CuckooHashing<T> {
 		return false;
 	}
 	
+	/**
+	 * Removes the element x from hashtable and marks the spot as deleted
+	 * @param x
+	 * @return the removed element x, null otherwise
+	 */
 	public Object remove(T x) {
 		if(contains(x)) {
 			for(int i=1; i<=numHashFunctions; i++ ) {
--- src/lxj171130/CuckooHashingDriver.java
+++ src/lxj171130/CuckooHashingDriver.java
@@ -1,12 +1,22 @@
+/**
+ * @author Leejia James
+ * @author Nirbhay Sibal
+ *
+ * Comparison of hashing implementations - Driver program
+ *
+ * Ver 1.0: 2018/10/20
+ */
 package lxj171130;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Scanner;
 
 public class CuckooHashingDriver {
 	public static void main(String[] args) throws FileNotFoundException {
+		// Performance of Cuckoo Hashing
 		CuckooHashing<Long> cuckooHashing = new CuckooHashing<>();
         Scanner sc;
         if (args.length > 0)
@@ -23,6 +33,8 @@ public class CuckooHashingDriver {
         long operand = 0;
         long result = 0;
         int modValue = 999983;
+
+        System.out.println("Cuckoo Hashing Performance:");
         // Initialize the timer
         Timer timer = new Timer();
 
@@ -57,12 +69,11 @@ public class CuckooHashingDriver {
         // End Time
         timer.end();
 
-        System.out.println("Cuckoo Hashing Performance:");
         System.out.println(result);
         System.out.println(timer);
         System.out.println();
         
-        // HashMap/Hashset
+        // Performance of Java's Hashset
         if (args.length > 0)
         {
             File file = new File(args[0]);
@@ -77,6 +88,8 @@ public class CuckooHashingDriver {
         operand = 0;
         result = 0;
         modValue = 999983;
+        	
+        System.out.println("Java Hashset Performance:");
         // Initialize the timer
         timer = new Timer();
         HashSet<Long> hs = new HashSet<>();
@@ -111,10 +124,61 @@ public class CuckooHashingDriver {
         // End Time
         timer.end();
         
-        System.out.println("Java Hashset Performance:");
         System.out.println(result);
         System.out.println(timer);
+        System.out.println();
+        
+        
+        //Generating an array of random integers and
+        //calculating how many distinct numbers it has
+        
+        Random rand = new Random();
+        int test = 10000;
+        int lowerBound = 10;
+        int upperBound = 100000;
+        Object[] arr1 = new Object[test];
+
+        for (int i1=0;i1<arr1.length;i1++) {
+          arr1[i1] = Math.abs(rand.nextInt(upperBound)-lowerBound);
+        }
+        System.out.println("Total numbers generated: "+ arr1.length);
+        System.out.println();
+        
+        System.out.println("Cuckoo Hashing Performance - Distinct numbers: ");
+        timer = new Timer();
+        int distinctCount = distinctElements(arr1);
+        timer.end();
+        System.out.println(timer);
+        System.out.println("Distinct: " + distinctCount);
+        System.out.println();
+
+        System.out.println("Java Hashset Performance - Distinct numbers: ");
+        HashSet<Object> hs1 = new HashSet<>();
+        timer = new Timer();
+        for (Object element : arr1) {
+          hs1.add(element);
+        }
+        timer.end();
+        System.out.println(timer);
+        System.out.println("Distinct: "+ hs1.size());
         
     }
+	
+	/**
+	 * Calculate the number of distinct elements in an array using the 
+	 * hashing implementation (Cuckoo Hashing)
+	 * @param arr
+	 * @return number of distinct elements
+	 */
+	static<T> int distinctElements(T[] arr) {
+		int count = 0;
+		CuckooHashing<T> cuckooHashing1 = new CuckooHashing<>();
+		for(T x: arr) {
+			if(cuckooHashing1.add(x)) {
+				count = count + 1;
+			}
+		}
+		return count;
+	}
 
 }
